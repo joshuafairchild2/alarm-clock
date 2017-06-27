5b4da298b6a4663dad8c4714b3acf3625ec0be34
joshua fairchild and tyler allen <joshua+tyler@>
2017-06-27 15:38:25 -0700

    fix bug regarding numbers such as '03' being turned into '3'

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -2,6 +2,40 @@ let Clock = require('./../js/clock.js').clockModule;
 
 
 $(() => {
+
   let currentClock = new Clock();
   setInterval(() => $('#time').text(currentClock.getTime()), 1000);
+
+  let alarm = null;
+  let iframe;
+  $(".alarm-form").submit((e) => {
+    e.preventDefault();
+    alarm = $("#alarm-input").val();
+    iframe = currentClock.soundArray.slice();
+    let hours = parseInt(alarm.substring(0, 2));
+    let minutes = parseInt(alarm.substring(3,5));
+
+    if (hours > 12) {
+      hours -= 12;
+    }
+    if(hours < 10) {
+      hours = ("0" + hours).slice(-2);
+    }
+    if(minutes < 10) {
+      minutes = ("0" + minutes).slice(-2);
+    }
+
+    alarm = `${hours}:${minutes}`;
+    currentClock.setAlarm(alarm);
+    console.log(currentClock.alarm);
+  });
+
+  setInterval(() => {
+    if (currentClock.alarmSounding(moment().format('h:mm'))) {
+      $("body").addClass('active-alarm');
+      $(".time").append(iframe.splice(0, 1));
+      setTimeout(() => $("body").removeClass('active-alarm'), 200);
+    }
+  }, 400);
+
 });
--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,23 @@
 function Clock() {
   this.alarm = null;
+  this.soundArray = ['<iframe width="0" height="0" src="https://www.youtube.com/embed/LQjoEvSd0Z0?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 }
 
-Clock.prototype.getTime = function(){
+Clock.prototype.getTime = function() {
   return moment().format("h:mm:ss a");
 };
+
+Clock.prototype.setAlarm = function(time) {
+  this.alarm = time;
+
+};
+
+Clock.prototype.alarmSounding = function(currentTime) {
+  if (currentTime == this.alarm) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 exports.clockModule = Clock;
