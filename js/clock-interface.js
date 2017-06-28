b85ec03d453c850e8b3fba8e25d3024437da8d06
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-06-27 18:52:11 -0700

    move some backend logic out of the front end

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -5,27 +5,12 @@ $(() => {
 
   let currentClock = new Clock();
   let iframe;
-  let alarm = null;
   setInterval(() => $('#time').text(currentClock.getTime()), 1000);
 
   $(".alarm-form").submit((e) => {
     e.preventDefault();
-    alarm = $("#alarm-input").val();
     iframe = currentClock.soundArray.slice();
-    let hours = parseInt(alarm.substring(0, 2));
-    let minutes = parseInt(alarm.substring(3,5));
-
-    if (hours > 12) {
-      hours -= 12;
-    }
-    if(hours < 10) {
-      hours = ("0" + hours).slice(-2);
-    }
-    if(minutes < 10) {
-      minutes = ("0" + minutes).slice(-2);
-    }
-
-    alarm = `${hours}:${minutes}`;
+    let alarm = $("#alarm-input").val();
     currentClock.setAlarm(alarm);
   });
 
--- js/clock.js
+++ js/clock.js
@@ -7,9 +7,24 @@ Clock.prototype.getTime = function() {
   return moment().format("h:mm:ss a");
 };
 
-Clock.prototype.setAlarm = function(time) {
-  this.alarm = time;
+Clock.prototype.setAlarm = function(timeToSet) {
+  let hours = parseInt(timeToSet.substring(0, 2));
+  let minutes = parseInt(timeToSet.substring(3,5));
 
+  if (hours > 12) {
+    hours -= 12;
+  }
+  if(hours < 10) {
+    hours = ("0" + hours).slice(-2);
+  }
+  if(minutes < 10) {
+    minutes = ("0" + minutes).slice(-2);
+  }
+
+  let alarm = `${hours}:${minutes}`;
+
+  this.alarm = alarm;
+  console.log(`Alarm set for ${this.alarm}`);
 };
 
 Clock.prototype.alarmSounding = function(currentTime) {
